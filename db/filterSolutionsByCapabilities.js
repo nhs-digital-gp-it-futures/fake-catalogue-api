92cdc5ca3c9f4ed64f8da24b77e7a23a2ebcb299
alibombaye <alibombaye@gmail.com>
2019-08-20 15:21:59 +0100

    handle when single selectedCapability

--- db/filterSolutionsByCapabilities.js
+++ db/filterSolutionsByCapabilities.js
@@ -1,12 +1,14 @@
 import solutions from './solutions';
 
 export const filterSolutionsByCapabilities = (selectedCapabilities) => {
+  const selectedCapabilitiesToArray = Array.isArray(selectedCapabilities) ? selectedCapabilities : [selectedCapabilities]
+
   const filteredSolutions = [];
 
   solutions.map(solution => {
     const listOfCapabilityIds = solution.capabilities.map(capability => capability.id);
 
-    const hasAllSelectedCapabilities = selectedCapabilities.every(selectedCapability => listOfCapabilityIds.indexOf(selectedCapability) > -1)
+    const hasAllSelectedCapabilities = selectedCapabilitiesToArray.every(selectedCapability => listOfCapabilityIds.indexOf(selectedCapability) > -1)
 
     if (hasAllSelectedCapabilities) {
       filteredSolutions.push(solution)
