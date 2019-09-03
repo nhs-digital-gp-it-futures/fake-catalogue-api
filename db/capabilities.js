7966228cb93fddff6a98485de3ef6133bdc53a3c
alibombaye <alibombaye@gmail.com>
2019-09-03 10:46:54 +0100

    expo heroku support

--- db/capabilities.js
+++ db/capabilities.js
@@ -1,50 +1,208 @@
 const capabilities = [
   {
-    "id": "C1",
-    "name": "Appointments Management - Citizen",
-    "description": "Enables Citizens to manage their Appointments online. Supports the use of Appointment slots that have been configured in Appointments Management - GP.",
-    "type": "foundation"
+    "id": "9302758D-CC42-47A7-8F66-0E009008E507",
+    "name": "Appointments Management - GP",
+    "isFoundation": true
   },
   {
-    "id": "C10",
-    "name": "GP Extracts Verification",
-    "description": "Supports Practice staff in ensuring accuracy of the data that is used with the Calculating Quality Reporting Service (CQRS).",
-    "type": "foundation"
-  },
-  {
-    "id": "C11",
-    "name": "Referral Management",
-    "description": "Supports recording, reviewing, sending, and reporting of Patient Referrals. Enables Referral information to be included in the Patient Record."
-  },
-  {
-    "id": "C12",
-    "name": "Resource Management",
-    "description": "Supports the management and reporting of Practice information, resources, Staff Members and related organisations. Also enables management of Staff Member availability and inactivity."
-  },
-  {
-    "id": "C13",
+    "id": "9F71BD15-E4BF-460C-9C2C-4C141B5B0727",
     "name": "Patient Information Maintenance",
-    "description": "Supports the registration of Patients and the maintenance of all Patient personal information. Supports the organisation and presentation of a comprehensive Patient Record. Also supports the management of related persons and configuring access to Citizen Services."
+    "isFoundation": true
   },
   {
-    "id": "C14",
+    "id": "669A02BE-BE1A-417D-8528-43C113C6E5E6",
     "name": "Prescribing",
-    "description": "Supports the effective and safe prescribing of medical products and appliances to Patients. Information to support prescribing will be available."
+    "isFoundation": true
   },
   {
-    "id": "C15",
+    "id": "A66C2751-72C1-4E9B-A504-51792EEE083E",
     "name": "Recording Consultations",
-    "description": "Supports the standardised recording of Consultations and other General Practice activities. Also supports the extraction of Female Genital Mutilation (FGM) data for the FGM data set."
+    "isFoundation": true
   },
   {
-    "id": "C16",
+    "id": "F6D02574-7232-48BD-8D45-7C145D47B4C7",
+    "name": "Referral Management",
+    "isFoundation": true
+  },
+  {
+    "id": "BDF84761-FB3D-4548-A9DC-E06965C1375A",
+    "name": "Resource Management",
+    "isFoundation": true
+  },
+  {
+    "id": "E4D22F8A-0F5F-43D5-A8E4-60F1365E968A",
+    "name": "Appointments Management - Citizen",
+    "isFoundation": false
+  },
+  {
+    "id": "FF04DF0E-2092-4D59-9D04-8CF4BBFD6E6A",
+    "name": "Care Homes",
+    "isFoundation": false
+  },
+  {
+    "id": "87E50CFD-9064-40B0-BECD-B8EA7F08AD56",
+    "name": "Caseload Management",
+    "isFoundation": false
+  },
+  {
+    "id": "83276C23-7564-4037-8EB0-7B87D5185AD2",
+    "name": "Clinical Decision Support",
+    "isFoundation": false
+  },
+  {
+    "id": "9E06E94D-9628-4526-88BA-A4CF26F997B6",
+    "name": "Communicate With Practice - Citizen",
+    "isFoundation": false
+  },
+  {
+    "id": "79519E22-E16F-47D8-B934-DD850D191CFF",
+    "name": "Communication Management",
+    "isFoundation": false
+  },
+  {
+    "id": "0E7C1FDA-167C-48BE-8665-200035DD532C",
+    "name": "Cross-organisation Appointment Booking",
+    "isFoundation": false
+  },
+  {
+    "id": "648D12EF-6A15-4DF4-B317-E9FC19567A42",
+    "name": "Cross-organisation Workflow Tools",
+    "isFoundation": false
+  },
+  {
+    "id": "B4261C6F-3413-4130-A685-02FCE7B6CF01",
+    "name": "Cross-organisation Workforce Management",
+    "isFoundation": false
+  },
+  {
+    "id": "ACA2968C-14FC-4573-9F9F-8B66D8A8454E",
+    "name": "Data Analytics for Integrated and Federated Care",
+    "isFoundation": false
+  },
+  {
+    "id": "3CE48C13-820B-411F-9671-88EA379BFBC8",
+    "name": "Digital Diagnostics",
+    "isFoundation": false
+  },
+  {
+    "id": "701DE56F-5DB4-4345-9E82-9E716208F71F",
+    "name": "Dispensing",
+    "isFoundation": false
+  },
+  {
+    "id": "25C81932-23B7-4407-A00E-8EFDD1EEA90D",
+    "name": "Document Management",
+    "isFoundation": false
+  },
+  {
+    "id": "39CDBEF1-FF29-46A0-8558-C584C4A0572C",
+    "name": "Domiciliary Care",
+    "isFoundation": false
+  },
+  {
+    "id": "B3379092-403E-43F6-A362-51D2F4BC12A3",
+    "name": "e-Consultations (Patient/Service User to Professional)",
+    "isFoundation": false
+  },
+  {
+    "id": "0B082164-F7CA-42D1-860B-782C8BE1BC27",
+    "name": "e-Consultations (Professional to Professional)",
+    "isFoundation": false
+  },
+  {
+    "id": "F4971AC2-D260-4F44-9A9B-D118A07D062D",
+    "name": "GP Extracts Verification",
+    "isFoundation": false
+  },
+  {
+    "id": "3B4B1FC3-720D-4814-A91D-6BB47EFAD393",
+    "name": "Medicines Optimisation",
+    "isFoundation": false
+  },
+  {
+    "id": "BF43E873-F5E7-47FA-BD86-7DF261121616",
+    "name": "Medicines Verification",
+    "isFoundation": false
+  },
+  {
+    "id": "A9D4A7F7-F024-4710-A93C-1C5206FE1839",
+    "name": "Personal Health Budget",
+    "isFoundation": false
+  },
+  {
+    "id": "E8E72A34-EA61-4098-A775-A93AD9DD8C93",
+    "name": "Personal Health Record",
+    "isFoundation": false
+  },
+  {
+    "id": "64FBEA9D-E74C-49E2-A006-712D0CBFC836",
+    "name": "Population Health Management",
+    "isFoundation": false
+  },
+  {
+    "id": "7EC2B363-D6B8-4DBA-9756-022784D8D940",
+    "name": "Prescription Ordering - Citizen",
+    "isFoundation": false
+  },
+  {
+    "id": "943AF712-5E13-4D33-BB93-807C06E9C988",
+    "name": "Productivity",
+    "isFoundation": false
+  },
+  {
+    "id": "3B6976A5-5532-41C6-89BF-92948BF238EF",
     "name": "Reporting",
-    "description": "Enables reporting and analysis of data from other Capabilities in the Practice Solution to support clinical care and Practice management."
+    "isFoundation": false
   },
   {
-    "id": "C17",
+    "id": "04F54A70-2640-4350-A22A-E7CF448A6A80",
+    "name": "Risk Stratification",
+    "isFoundation": false
+  },
+  {
+    "id": "BB6F4DC1-C54E-4485-97AE-4BB73AD8EC2E",
     "name": "Scanning",
-    "description": "Support the conversion of paper documentation into digital format preserving the document quality and structure."
+    "isFoundation": false
+  },
+  {
+    "id": "B521B49E-116A-46CE-A675-832127A33C7E",
+    "name": "Shared Care Plans",
+    "isFoundation": false
+  },
+  {
+    "id": "01CABDFA-3EA2-4AFC-B02B-EA22FA98D49C",
+    "name": "Social Prescribing",
+    "isFoundation": false
+  },
+  {
+    "id": "B8DD2E0D-33F4-4A63-8F2D-7B5ACB10C57D",
+    "name": "Telecare",
+    "isFoundation": false
+  },
+  {
+    "id": "17A78A9C-F726-4AF0-9F97-A833BE7DF6A4",
+    "name": "Telehealth",
+    "isFoundation": false
+  },
+  {
+    "id": "3A7B5E60-60C8-4AFE-8756-6BFFB89E90EF",
+    "name": "Unified Care Record",
+    "isFoundation": false
+  },
+  {
+    "id": "C2E2117B-3603-42A2-81C2-A94B3BE793EF",
+    "name": "Unstructured Data Extraction",
+    "isFoundation": false
+  },
+  {
+    "id": "316EF028-56EA-4A49-A3E0-7E901FBCFE7E",
+    "name": "View Record - Citizen",
+    "isFoundation": false
+  },
+  {
+    "id": "0FC6D728-8BC8-4D6F-B606-038B15040BF9",
+    "name": "Workflow",
+    "isFoundation": false
   }
 ];
 
